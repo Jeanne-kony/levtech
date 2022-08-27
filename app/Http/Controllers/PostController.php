94493fdf274f5867fa37c61f367a7789dca08844
Jeanne-kony <lull-the.earth-color@outlook.jp>
2022-08-27 11:30:46 +0900

    途中

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,11 @@ class PostController extends Controller
 {
     public function index(Post $post)
    {
-    return $post->get();
+    return view('index')->with(['posts' => $post->getPaginate()]);
+   }
+   
+   public function show(Post $post)
+   {
+       return view('show')->with(['post' => $post]);
    }
 }
